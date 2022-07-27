7eb454c7f44c02629175c6d758af458bcd4aa908
Halyna Melnykova <galyamelnykova@gmail.com>
2022-07-27 17:24:28 +0300

    add svg add styles

--- src/js/discovery-api.js
+++ src/js/discovery-api.js
@@ -27,7 +27,7 @@ export default class DiscoveryEventsSerch {
 // ф-я для модалки
   async discoveryModalSerch() {
     //с помощью бибиотеки axios, делаем запрос, в котором 2-м аргументом прокидываем параметры запроса
-    const request = await axios.get(`${this.#SECOND_URL}/Z7r9jZ1AdFw7g`, {
+    const request = await axios.get(`${this.#SECOND_URL}/k7vGF9anxhJp5`, {
       params: {
         apikey: this.#API_KEY,
         // id: 'Z7r9jZ1AdFw7g',
--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -26,21 +26,36 @@ response.then(results => {
   objModal.name = results.data.name;
   objModal.date = results.data.dates.start.localDate;
   objModal.time = results.data.dates.start.localTime;
-  objModal.image = results.data.images[6];
-  objModal.info = 'will be avalible soon';
-
-  //  objModal = {
-  //   name: results.data.name,
-  //   date: results.data.dates.start.localDate,
-  //   time: results.data.dates.start.localTime,
-  //   image: results.data.images[6],
-  //   info: 'will be avalible soon',
-
+  objModal.image = results.data.images[1].url;
+  objModal.imageBig = results.data.images[7].url;
+  // objModal.info = 'will be avalible soon';
+  // if (!objModal.info) {
+  //   objModal.info = 'will be avalible soon';
+  // } 
+  objModal.info = results.data.info;
+  objModal.city = results.data._embedded.venues[0].city.name;
+  objModal.country = results.data._embedded.venues[0].country.name;
+  objModal.location = results.data._embedded.venues[0].name;
+  // if (!objModal.priceMin) {
+  //   objModal.priceMin = 'will be avalible soon';
   // }
+  objModal.priceMin = results.data.priceRanges[0].min;
+  objModal.priceMax = results.data.priceRanges[0].max;
+  objModal.currency = results.data.priceRanges[0].currency;
+  objModal.link = results.data.url;
+
+// else {
+//   objModal.info = results.data.info;
+// }
+//  else {
+//   objModal.priceMin = results.data.priceRanges[0].min;
+// }
+
   console.log(objModal)
   return objModal;
-  //  results.data._embedded.events
 });
+
+
 
 const modalEl = document.querySelector('.js-modal-card');
 import markupModal from '../templates/modal.hbs';
